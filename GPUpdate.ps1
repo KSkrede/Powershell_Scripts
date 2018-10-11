3958dfba640226cfd0cfc14e507239a3ab8537ea
Skrede <kristian.skrede@marinit.no>
2018-10-11 10:10:12 +0200

    Testing

---
+++ GPUpdate.ps1
@@ -0,0 +1,17 @@
+$RemoteComputers = Get-Content -Path C:\Users\kriskr_admin\Documents\PCList2.txt
+$timer = (Get-Date -Format yyyy-MM-dd-HHmm)
+$filename = "C:\Users\kriskr_admin\Documents\" + $timer + "_error_log.txt"
+$KasperskyNuke = msiexec.exe /x {7A4192A1-84C4-4E90-A31B-B4847CA8E23A} KLLOGIN=KLAdmin KLPASSWD=Kasperthefr13ndlygh0st
+ForEach ($Computer in $RemoteComputers)
+{
+     Try
+         {
+          Invoke-Command -ComputerName $Computer -ScriptBlock { Start-Process -FilePath "C:\Windows\SysWOW64\msiexec.exe" -ArgumentList "/x {7A4192A1-84C4-4E90-A31B-B4847CA8E23A} KLLOGIN=KLAdmin KLPASSWD=Kasperthefr13ndlygh0st"} -ErrorAction Stop
+         }
+     Catch
+         {
+             Add-Content $filename $Computer
+             
+         }
+             
+}
