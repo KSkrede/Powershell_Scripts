99b2ebbb0179513c90574916fbec67716085f17c
Skrede <kristian.skrede@marinit.no>
2018-10-11 11:25:35 +0200

    commit test

---
+++ GPUpdate.ps1
@@ -0,0 +1,19 @@
+$RemoteComputers = Get-Content -Path C:\Users\kriskr_admin\Documents\PCList.txt
+$timer = (Get-Date -Format yyyy-MM-dd-HHmm)
+$filename = "C:\Users\kriskr_admin\Documents\" + $timer + "_error_log.txt"
+ForEach ($Computer in $RemoteComputers)
+{
+     Try
+         {
+            Invoke-GPUpdate -Computer $Computer -Force 
+         }
+     Catch
+         {
+             Add-Content $filename $Computer
+             
+         }
+             
+}
+
+
+Dette er en test2
