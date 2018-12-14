2779fef156486e79af2700dd59dff477af303706
Skrede, Kristian <Kristian@skrede.info>
2018-12-14 09:53:26 +0100

    Added what if remove function

--- Filter out mails from distributiongroup.ps1
+++ Filter out mails from distributiongroup.ps1
@@ -14,8 +14,9 @@ foreach ($TargetDG in $COMPDG) {
 #Find all memebers and filter to those who follow the criteria
 Get-DistributionGroupMember -Identity $TargetDG | Where PrimarySmtpAddress -notlike *@mail1.com | Where PrimarySmtpAddress -notlike *mail2.com | 
 #Write to the document
- ForEach-Object {
-				$_.Name + "  Email:(" + $_.PrimarySMTPAddress + ")" | Add-Content $saveto
+ ForEach-Object { Remove-DistributionGroupMember -Identity $TargetDG -Member $_.Name -WhatIf 
+
+	$_.Name + "  Email:(" + $_.PrimarySMTPAddress + ")" +" Deleted from the group" + $TargetDG | Add-Content $saveto
 			    }
 	}
 
