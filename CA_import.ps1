33ad9d9f689225f4f96c8c9599091bd0e3843a6d
Kristian Skrede <44050668+KSkrede@users.noreply.github.com>
2021-10-07 17:28:50 +0200

    Intune exports

---
+++ CA_import.ps1
@@ -0,0 +1,25 @@
+# Take cmd parameters
+param(
+    [parameter()]
+    [String]$BackupPath
+)
+
+#Creates folder if it does not exists
+
+If (!(test-path $BackupPath))
+{
+    md $BackupPath
+}
+
+# Connect to Azure AD
+Connect-AzureAD
+
+$AllPolicies = Get-AzureADMSConditionalAccessPolicy
+
+foreach ($Policy in $AllPolicies) {
+    Write-Output "Backing up $($Policy.DisplayName) to ($BackupPath) "
+    $PolicyJSON = $Policy | ConvertTo-Json -Depth 6
+    $PolicyJSON | Out-File "$BackupPath\$($Policy.DisplayName).json"
+}
+$Policycombined = $AllPolicies | ConvertTo-Json -Depth 6
+$Policycombined | Out-File "$BackupPath\combined.json"
